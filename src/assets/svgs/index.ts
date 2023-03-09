d85cee02ee4f641e084a82078b0bdaec54a836a6
Sami Ullah <sami.naazar1526@gmail.com>
2023-03-10 00:42:00 +0500

    Add svgs

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,18 @@
+import SunSvg from "./sunSvg";
+import MoonSvg from "./moonSvg";
+import EyeSvg from "./eyeSvg";
+import SideBoxSvg from "./sideBoxSvg";
+import MobileScreenLogoSvg from "./mobileScreenLogoSvg";
+import LogoDarkSvg from "./logoDarkSvg";
+import LogoLightSvg from "./logoLightSvg";
+
+
+export {
+    SunSvg,
+    MoonSvg,
+    EyeSvg,
+    SideBoxSvg,
+    MobileScreenLogoSvg,
+    LogoDarkSvg,
+    LogoLightSvg,
+}
